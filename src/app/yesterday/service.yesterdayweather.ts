1ff573fa052dc6ddcdb25717fb744ea6a0122a9d
lui697 <kranti.chintala@capitalone.com>
2017-10-10 16:19:58 -0400

    feat(HighCharts): Charts for temperature are implemented.

--- src/app/yesterday/service.yesterdayweather.ts
+++ src/app/yesterday/service.yesterdayweather.ts
@@ -4,18 +4,21 @@ import { Http , Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
+const Highcharts = require('highcharts/highcharts.src');
+import 'highcharts/adapters/standalone-framework.src';
 
 @Injectable()
 export class YesterDayWeatherService {
     //http://api.wunderground.com/api/8d55b1fea347ef26/forecast/q/CA/San_Francisco.json
 
    private _forecastWeatherUrl = 'http://api.wunderground.com/api/8d55b1fea347ef26/yesterday/q/CA/San_Francisco.json';
-   constructor(private _http: Http) {
-   }
 
-   getYesterdayData(): Observable<IYesterday> {
-      return this._http.get(this._forecastWeatherUrl)
-      .map((response: Response) => <IYesterday> response.json());
-      //.do(data => console.log(data));
-   }
+  getYesterdayData(): Observable<IYesterday> {
+    return this._http.get(this._forecastWeatherUrl)
+    .map((response: Response) => <IYesterday> response.json());
+    //.do(data => console.log(data));
+ }
+  constructor(private _http: Http) {
+}
+
 }
